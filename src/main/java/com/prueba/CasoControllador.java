98973066f366f84c216b58093ebcaa20f1d7b90b
Alex Mancilla <>
2020-05-09 11:15:22 -0500

    dddd

---
+++ src/main/java/com/prueba/CasoControllador.java
@@ -0,0 +1,27 @@
+package com.prueba;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@RestController
+public class CasoControllador {
+    @Autowired
+    CasoService casoService;
+
+
+    @GetMapping("/agregate/{hasta}/{anno}")
+    public void agregate(@PathVariable("hasta") int hasta,@PathVariable("anno") int anno) throws IOException {
+        casoService.agregate(hasta,anno);
+    }
+
+
+    @GetMapping("/insertar/{hasta}")
+    public void insertar(@PathVariable("hasta") int hasta) throws IOException {
+        casoService.insertar(hasta);
+    }
+}
---
+++ src/main/java/com/prueba/MongoDbSettings.java
@@ -0,0 +1,22 @@
+package com.prueba;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoDbSettings {
+
+//    @Bean
+//    public MongoClientOptions mongoOptions() {
+//
+//        int minuto = 90000000*10;
+//
+//
+//        return MongoClientOptions.builder()
+//                .socketKeepAlive(true)
+//                .socketTimeout(minuto)
+//                .serverSelectionTimeout(minuto)
+//                .connectTimeout(minuto)
+//                .build();
+//    }
+
+}
